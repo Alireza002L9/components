78719de2d63ccffaecafa27937f8335d5d04f848
Alireza Esmaeili <alirezaesmaili6036@gmail.com>
2023-09-21 19:04:14 +0330

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Something from './components/something'
 import Form from './components/form'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <Something />
       <Form />
     </div>
